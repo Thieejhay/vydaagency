b560c1263df8f9a1085971f1988c0e493deebe75
Tijani Tofunmi <tijani.tofunmi@gmail.com>
2023-09-11 11:20:51 +0100

    updated commits

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -3,11 +3,12 @@ import Icon from '../assets/Icon1.png';
 import Icon2 from '../assets/Icon2.png';
 import Icon3 from '../assets/Icon3.png';
 import {BsDot} from 'react-icons/bs';
+import { HashLink as Link } from 'react-router-hash-link';
 
 
 const Services = () => {
   return (
-    <div className='flex flex-col w-screen bg-[#ffffff] lg:pt-20 pt-12 lg:pb-12 pb-10 lg:gap-10 gap-5 font-gilroy' id='services'>
+    <div className='flex flex-col w-screen bg-[#ffffff] lg:pt-20 pt-12 lg:pb-12 pb-7 lg:gap-10 gap-5 font-gilroy' id='services'>
         <div className='flex flex-col lg:gap-3 gap-2 w-full items-center'>
             <span className='uppercase lg:text-4xl text-2xl font-black text-[#273773] font-gilroyy'>list of services</span>
             <span className='text-base font-light text-[#000000]/60 font-gilroy text-center lg:px-32 px-4'>Explore our extensive array of services, which has been professionally handpicked to help you enhance your company and succeed in today's changing environment. Learn how our strategic excellence, innovation, and unrelenting dedication can fuel your success.</span>
@@ -114,9 +115,11 @@ const Services = () => {
                 </div>
             </div>
         </div>
-        <div className='flex justify-end pr-32 lg:-mt-4 -mt-6 md:-mt-2'>
-            <span className='p-4 text-base font-normal bg-[#273773] text-[#ffffff] rounded-full w-fit'>Learn more</span>
-        </div>
+        <Link to='#contact' smooth>
+            <div className='flex lg:justify-end justify-center lg:pr-32 lg:-mt-4 md:-mt-2'>
+                <span className='p-4 text-base font-normal bg-[#273773] text-[#ffffff] rounded-full w-fit'>Learn more</span>
+            </div>
+        </Link> 
     </div>
   )
 }
